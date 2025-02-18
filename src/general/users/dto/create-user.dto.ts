6035ecb1803808d9bfd8804333ac726694beee9d
Yerkow <112828364+yerkow@users.noreply.github.com>
2025-02-18 17:54:05 +0500

    refactor: center_id is Optional parameter

--- src/general/users/dto/create-user.dto.ts
+++ src/general/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { Type } from "class-transformer";
+import { Type } from 'class-transformer'
 import {
   IsNumber,
   IsObject,
@@ -7,44 +7,45 @@ import {
   IsString,
   MaxLength,
   MinLength,
-  ValidateNested,
-} from "class-validator";
+  ValidateNested
+} from 'class-validator'
 
 class ProfileDto {
   @IsString()
   @MaxLength(12)
   @MinLength(12)
-  iin: string;
+  iin: string
 
   @IsString()
   @MinLength(2)
-  full_name: string;
+  full_name: string
 
-  @IsPhoneNumber("KZ")
-  phone: string;
+  @IsPhoneNumber('KZ')
+  phone: string
 }
 
 export class CreateUserDto {
   @IsString()
-  login: string;
+  login: string
 
   @IsString()
   @MinLength(6)
   @MaxLength(20)
-  password: string;
+  password: string
 
   @IsObject()
   @ValidateNested({ each: true })
   @Type(() => ProfileDto)
-  profile: ProfileDto;
+  profile: ProfileDto
 
   @IsNumber()
-  role: number;
+  role: number
 
+  @IsOptional()
   @IsNumber()
-  center_id: number;
+  center_id?: number
 
   @IsOptional()
   @IsNumber({}, { each: true })
-  service_ids?: number[];
+  service_ids?: number[]
 }
