505935de5c6171faae10f2964cc5dbf2edea20ed
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-03-14 04:45:11 +0500

    config Сид для генерации сервисов к типам посетителей

---
+++ seeders/20250123130331-generate-service-visitor-types.js
@@ -0,0 +1,89 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert('service_visitor_types', [
+      {
+        service_id: 1,
+        visitor_type_id: 1
+      },
+      {
+        service_id: 1,
+        visitor_type_id: 2
+      },
+      {
+        service_id: 2,
+        visitor_type_id: 1
+      },
+      {
+        service_id: 2,
+        visitor_type_id: 2
+      },
+      {
+        service_id: 3,
+        visitor_type_id: 1
+      },
+      {
+        service_id: 3,
+        visitor_type_id: 2
+      },
+      {
+        service_id: 4,
+        visitor_type_id: 1
+      },
+      {
+        service_id: 4,
+        visitor_type_id: 2
+      },
+      {
+        service_id: 5,
+        visitor_type_id: 1
+      },
+      {
+        service_id: 5,
+        visitor_type_id: 2
+      },
+      {
+        service_id: 6,
+        visitor_type_id: 1
+      },
+      {
+        service_id: 6,
+        visitor_type_id: 2
+      },
+      {
+        service_id: 7,
+        visitor_type_id: 1
+      },
+      {
+        service_id: 7,
+        visitor_type_id: 2
+      },
+      {
+        service_id: 8,
+        visitor_type_id: 1
+      },
+      {
+        service_id: 8,
+        visitor_type_id: 2
+      },
+      {
+        service_id: 9,
+        visitor_type_id: 1
+      },
+      {
+        service_id: 10,
+        visitor_type_id: 1
+      },
+      {
+        service_id: 11,
+        visitor_type_id: 1
+      }
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('service_visitor_types', null, {})
+  }
+}
